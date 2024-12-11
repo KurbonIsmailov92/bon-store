2a32b4fe0c052b12818a56766dc8b4725f5e7b00
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2024-12-11 16:47:17 +0500

    refactor thumbnails

---
+++ app/Support/Testing/FakerImageProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Support\Testing;
+
+use Faker\Provider\Base;
+use Illuminate\Support\Facades\Storage;
+
+final class FakerImageProvider extends Base
+{
+    public function fixturesImage(string $fixturesDir, string $storageDir): string
+    {
+        if (!Storage::exists($storageDir)) {
+            Storage::makeDirectory($storageDir);
+        }
+
+        $file = $this->generator->file(
+            base_path("tests/fixtures/images/$fixturesDir"),
+            Storage::path($storageDir), false
+        );
+        return '/storage/' . trim($storageDir, '/') . '/' . $file;
+    }
+}
