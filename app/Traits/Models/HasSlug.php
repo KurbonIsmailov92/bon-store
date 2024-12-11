e82b1dc1add850f7328c73f3c61d0dee75874754
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2024-12-11 16:14:25 +0500

    refactor HasSlug

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -9,16 +9,49 @@ trait HasSlug
     protected static function bootHasSlug(): void
     {
         static::creating(function (Model $item) {
-            $item->slug = $item->slug
-                ?? str($item->{self::slugFrom()})
-                    ->append(rand(1, 100))
-                    ->slug();
+            $item->makeSlug();
         });
     }
 
-    public static function slugFrom(): string
+    protected function makeSlug(): void
+    {
+        $slug = $this->slugUnique(
+            str($this->{$this->slugFrom()})
+                ->slug()
+                ->value()
+        );
+        $this->{$this->slugColumn()} = $this->{$this->slugColumn()} ?? $slug;
+    }
+
+    protected function slugColumn(): string
+    {
+        return 'slug';
+    }
+
+    protected function slugFrom(): string
     {
         return 'title';
+    }
+
+    private function slugUnique(string $slug)
+    {
+        $originalSlug = $slug;
+        $i = 0;
+
+        while ($this->isSlugExists($slug)) {
+            $i++;
+            $slug = $originalSlug . "-" . $i;
+        }
+        return $slug;
+    }
+
+    private function isSlugExists(string $slug)
+    {
+        $query = $this->newQuery()
+            ->where(self::slugColumn(), $slug)
+            ->where($this->getKeyName(), '!=', $this->getKey())
+            ->withoutGlobalScopes();
+        return $query->exists();
     }
 
 }
